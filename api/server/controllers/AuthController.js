e4b142e196240f95b723484ead20c257841cf7ce
oviemudi <oviisaacs@gmail.com>
2021-04-13 12:36:53 +0100

    quickfix for routing

--- api/server/controllers/AuthController.js
+++ api/server/controllers/AuthController.js
@@ -3,25 +3,25 @@ const userService = require('../services/UserService');
 const AuthService = require('../services/authService');
 const util = new Util();
 
-
 const { regValidation, loginValidation } = require('../validators/validate');
 
 class AuthController {
-
   static async signUp(req, res) {
-    // Joi checks User Input
-    // const validateInput = regValidation.validate(req.body);
-    // if (validateInput.error) {
-    //   throw (validateInput.error.message, 401);
-    // }
-    console.log("iiiiiiiiiiiiiiiiiiiiii")
+    console.log('iiiiiiiiiiiiiiiiiiiiii');
+    try {
+      // Joi checks User Input
+      const validateInput = regValidation.validate(req.body);
+      if (validateInput.error) {
+        // return util.setError (validateInput.error.message, 401);
+        util.setError(400, validateInput.error.message);
+        return util.send(res);
+      }
 
-
-    const result = await AuthService.signup(req.body);
-    res.status(201).send(response("User created", result));
+      const result = await AuthService.signup(req.body);
+      res.status(201).send(response('User created', result));
+    } catch (error) {
+      return util.setError(409, 'Something went wrong');
+    }
   }
-
-
-
-};
+}
 module.exports = AuthController;
