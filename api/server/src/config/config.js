049c365383f55876bd80fb3cffe4d6f8ceae4814
Kasie Omeh <omeh.kasie@gmail.com>
2021-04-29 00:23:35 +1000

    package.json

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -23,12 +23,12 @@ module.exports = {
   },
 
   production: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
+    // database: "bookclub",
+    // username: "postgres",
+    // password: "Lookup0806Kelvin",
+    // host: "127.0.0.1",
+    // port: 5432,
+    // dialect: "postgres",
+    use_env_variable: "DATABASE_URL",
   },
-  
-  
 };
