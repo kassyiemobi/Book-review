61e69ccb025d841f1eec5c0e7d3bb54ba5811f8a
Kasie Omeh <omeh.kasie@gmail.com>
2021-05-01 23:02:26 +1000

    heroku

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -1,3 +1,5 @@
+const { required } = require("joi");
+
 require("dotenv").config();
 
 module.exports = {
@@ -30,5 +32,6 @@ module.exports = {
     // port: 5432,
     // dialect: "postgres",
     use_env_variable: "DATABASE_URL",
+    ssl:true
   },
 };
