1b8cf1628c827fdc8b7db8fc0f4b05d37cb7eed5
Kasie Omeh <omeh.kasie@gmail.com>
2021-05-08 00:43:26 +1000

    heroku

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
 
   production: {
-    use_env_variable: DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialect: "postgres",
     dialectOptions: {
       ssl: {
