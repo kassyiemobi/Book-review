cff776164db1c3a35a6295bafc74f80845f15892
Kasie Omeh <omeh.kasie@gmail.com>
2021-05-08 06:52:52 +1000

    heroku fix

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -22,9 +22,13 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "postgres",
   },
-
+  //postgres://rhpdmrhrbptsmg:20a08034021650b7347678cd2cf5910c517b36ffebd10d26116c6bb36eee9eb9@ec2-3-211-37-117.compute-1.amazonaws.com:5432/dc33n2epfolsf7
   production: {
-    use_env_variable: 'DATABASE_URL',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
